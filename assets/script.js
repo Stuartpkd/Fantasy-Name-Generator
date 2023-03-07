9f4e65de660834087932bdf58e5a43f55b5d7649
Stuartpkd <stuartparkerdesign@gmail.com>
2023-03-07 14:32:38 +0000

    Add more names to lists

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-const firstNames = ['Barka', 'Treath', 'Fring', 'Bela', 'Crach', 'Darwn', 'Gawb', 'Thrain', 'Gloin', 'Fasem', 'Beara', 'Coraw']
-const lastNames = ['thir', 'bara', 'graw', 'binir', 'dreathe', 'nawk', 'pown', 'minas', 'moria', 'frun', 'morgo', 'aswan']
+const firstNames = ['Barka', 'Treath', 'Fring', 'Bela', 'Crach', 'Darwn', 'Gawb', 'Thrain', 'Gloin', 'Fasem', 'Beara', 'Coraw', 'Breda', 'Lego', 'Tre', 'Vinir', 'Chrono', 'harfa', 'Brini', 'Zene']
+const lastNames = ['thir', 'bara', 'graw', 'binir', 'dreathe', 'nawk', 'pown', 'minas', 'moria', 'frun', 'morgo', 'aswan', 'thir', 'rena', 'thor', 'tos', 'war', 'las', 'greath', 'born', 'krek', 'bar', 'grun']
 
 const randomNumber = (max) => Math.floor(Math.random() * max);
 
